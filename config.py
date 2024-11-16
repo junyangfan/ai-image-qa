99a3591ac0ad942263222b42e4557f66c4937c09
fanjunyang <fanjunyang@daojia-inc.com>
2024-11-16 15:57:52 +0800

    docs: 修改配置

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # 通用配置
 
-# 截图区域配置 (x, y, width, height)
+# 屏幕截图区域配置 (x, y, width, height)
 SCREENSHOT_REGION = (100, 280, 300, 400)
 # 描述
 IMAGE_QA = "提取出图片中的问题和选项，从A、B、C、D四个选项中选择一个正确的答案，并给出选择的原因。"
@@ -10,6 +10,7 @@ CAPTURE_INTERVAL = 5
 
 
 # 星火大模型API的配置，申请地址：https://xinghuo.xfyun.cn/sparkapi
+# 修改 SPARKAI_APP_ID、SPARKAI_API_SECRET、SPARKAI_API_KEY 参数，SPARKAI_URL、SPARKAI_DOMAIN 参数无需修改
 SPARKAI_APP_ID = 'appId'
 SPARKAI_API_SECRET = 'apiSecret'
 SPARKAI_API_KEY = 'apiKey'
@@ -18,7 +19,7 @@ SPARKAI_DOMAIN = 'image'
 
 
 # 通义千问大模型API的配置，文档/申请地址：https://help.aliyun.com/zh/model-studio/user-guide/vision
+# 修改 ALIYUN_API_KEY 参数，ALIYUN_BASE_URL、ALIYUN_MODEL 参数无需修改
+ALIYUN_API_KEY = 'apiKey'
 ALIYUN_BASE_URL = 'https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions'
 ALIYUN_MODEL = 'qwen-vl-max-latest'
-ALIYUN_API_KEY = 'apiKey'
-
